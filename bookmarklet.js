8efef390a824797e83e9a24f01895c95af2e6a0f
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-09-29 10:44:33 -0500

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function(){script = document.createElement("script");script.src="https://cdn.jsdelivr.net/gh/RuralAnemone/screenshot@master/script.js";document.body.appendChild(script)}())
