acc5be223d3fee9cbf91df4ac431c64407cc5eef
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-09-29 20:26:56 -0500

    save draft (not done)

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const screenshotTarget = document.body;
-var script = document.createElement('script'); script.src = "https://html2canvas.hertzen.com/dist/html2canvas.min.js"; document.body.appendChild(script); script.onload = function() {
+
+var css = document.createElement('style');
+css.src = "https://html2canvas.hertzen.com/dist/html2canvas.min.js";
+document.body.appendChild(css);
+
+var jswindow = document.createElement('script');
+jswindow.src = "https://cdn.jsdelivr.net/gh/BinBashBanana/jswindow@master/jswindow.js";
+document.body.appendChild(jswindow);
+
+var h2c = document.createElement('script');
+h2c.src = "https://html2canvas.hertzen.com/dist/html2canvas.min.js";
+document.body.appendChild(h2c);
+
+h2c.onload = function() {
   html2canvas(screenshotTarget).then((canvas) => {
     const base64image = canvas.toDataURL("image/png");
     options(base64image);
@@ -7,19 +20,11 @@ var script = document.createElement('script'); script.src = "https://html2canvas
   });
 }
 
-function options(a) {
-  var r;
+function options(image) {
   if (window.confirm('copy the image url?')) {
-    navigator.clipboard.writeText(a).catch(e=>alert(`error: ${e}`));
-    r++;
+    navigator.clipboard.writeText(image).catch(e=>alert(`error: ${e}`));
   }
-  
-  if (window.confirm('redirect to image?')) {
-    window.location.href = a;
-    r++;
-  }
-  
-  if (r != 0) {
-    alert('oh ok\nI guess not then :)')
-  }
+  let w = new jswindow({title: "Generated screenshot:", icon: "https://assets.stickpng.com/images/580b585b2edbce24c47b2a2c.png"});
+  w.innerWindow.innerHTML = '<img src="' + image + '"></img>';
+  w.open({width: 200, height: 200});
 }
