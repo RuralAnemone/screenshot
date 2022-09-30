34e07df6a15fe0df5e98fdb9561b4292cba7c15c
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-09-30 17:00:13 -0500

    got the window to work :)

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const screenshotTarget = document.body;
 
-var css = document.createElement('style');
-css.src = "https://html2canvas.hertzen.com/dist/html2canvas.min.js";
-document.body.appendChild(css);
+var jswindowScript = document.createElement('script');
+jswindowScript.src = "https://cdn.jsdelivr.net/gh/BinBashBanana/jswindow@master/jswindow.js";
+document.body.appendChild(jswindowScript);
 
-var jswindow = document.createElement('script');
-jswindow.src = "https://cdn.jsdelivr.net/gh/BinBashBanana/jswindow@master/jswindow.js";
-document.body.appendChild(jswindow);
+var jswindowCss = document.createElement('link');
+jswindowCss.href = "https://cdn.jsdelivr.net/gh/BinBashBanana/jswindow@master/jswindow.css";
+jswindowCss.rel = "stylesheet";
+document.body.appendChild(jswindowCss);
 
 var h2c = document.createElement('script');
 h2c.src = "https://html2canvas.hertzen.com/dist/html2canvas.min.js";
@@ -21,10 +22,7 @@ h2c.onload = function() {
 }
 
 function options(image) {
-  jswindow.onload = function() {
-    console.log("jswindow loaded");
-    let w = new jswindow({title: "Generated screenshot:", icon: "https://assets.stickpng.com/images/580b585b2edbce24c47b2a2c.png"});
-    w.innerWindow.innerHTML = '<img src="' + image + '"></img>';
-    w.open({width: 200, height: 200});
-  }
+  let w = new jswindow({title: "Generated screenshot:", icon: "https://assets.stickpng.com/images/580b585b2edbce24c47b2a2c.png"});
+  w.innerWindow.innerHTML = '<img src="' + image + '"></img>';
+  w.open({width: 700, height: 300});
 }
